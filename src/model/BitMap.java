9eabd337565048bf1483f47cd360140db0b012b0
Ssolaris <santieltroll.2003@gmail.com>
2022-09-20 17:12:51 -0500

    Ahora si, version final, con foto del koala

--- src/model/BitMap.java
+++ src/model/BitMap.java
@@ -5,48 +5,6 @@ import java.io.IOException;
 
 public class BitMap {
 
-<<<<<<< HEAD
-	public int[] readBitmap(String path) throws IOException {
-		FileInputStream file = new FileInputStream(path);
-		byte[] headerBytes = new byte[56];
-		int[] headerInfo = new int[16];
-		file.read(headerBytes);
-		file.close();
-		byte[] bfTypeBytes = Arrays.copyOfRange(headerBytes, 0, 2);
-		headerInfo[0] = bytesToShort(reverseArray(bfTypeBytes));
-		byte[] bfSizeBytes = Arrays.copyOfRange(headerBytes, 2, 6);
-		headerInfo[1] = bytesToInt(reverseArray(bfSizeBytes));
-		byte[] bfReserved1Bytes = Arrays.copyOfRange(headerBytes, 6, 8);
-		headerInfo[2] = bytesToShort(reverseArray(bfReserved1Bytes));
-		byte[] bfReserved2Bytes = Arrays.copyOfRange(headerBytes, 8, 10);
-		headerInfo[3] = bytesToShort(reverseArray(bfReserved2Bytes));
-		byte[] bfOffBitsBytes = Arrays.copyOfRange(headerBytes, 10, 14);
-		headerInfo[4] = bytesToInt(reverseArray(bfOffBitsBytes));
-		byte[] biSizeBytes = Arrays.copyOfRange(headerBytes, 14, 18);
-		headerInfo[5] = bytesToInt(reverseArray(biSizeBytes));
-		byte[] biWidthBytes = Arrays.copyOfRange(headerBytes, 18, 22);
-		headerInfo[6] = bytesToInt(reverseArray(biWidthBytes));
-		byte[] biHeightBytes = Arrays.copyOfRange(headerBytes, 22, 26);
-		headerInfo[7] = bytesToInt(reverseArray(biHeightBytes));
-		byte[] biPlanesBytes = Arrays.copyOfRange(headerBytes, 26, 28);
-		headerInfo[8] = bytesToShort(reverseArray(biPlanesBytes));
-		byte[] biBitCountBytes = Arrays.copyOfRange(headerBytes, 28, 30);
-		headerInfo[9] = bytesToShort(reverseArray(biBitCountBytes));
-		byte[] biCompressionBytes = Arrays.copyOfRange(headerBytes, 30, 34);
-		headerInfo[10] = bytesToInt(reverseArray(biCompressionBytes));
-		byte[] biSizeImageBytes = Arrays.copyOfRange(headerBytes, 34, 38);
-		headerInfo[11] = bytesToInt(reverseArray(biSizeImageBytes));
-		byte[] biXPelsPerMeterBytes = Arrays.copyOfRange(headerBytes, 38, 42);
-		headerInfo[12] = bytesToInt(reverseArray(biXPelsPerMeterBytes));
-		byte[] biYPelsPerMeterBytes = Arrays.copyOfRange(headerBytes, 42, 46);
-		headerInfo[13] = bytesToInt(reverseArray(biYPelsPerMeterBytes));
-		byte[] biClrUsedBytes = Arrays.copyOfRange(headerBytes, 46, 50);
-		headerInfo[14] = bytesToInt(reverseArray(biClrUsedBytes));
-		byte[] biCrlImportantBytes = Arrays.copyOfRange(headerBytes, 50, 54);
-		headerInfo[15] = bytesToInt(reverseArray(biCrlImportantBytes));
-		return headerInfo;
-	}
-=======
     public int[] readBitmap() throws IOException {
         FileInputStream file = new FileInputStream("image.bmp");
         byte[] headerBytes = new byte[54];
@@ -88,7 +46,7 @@ public class BitMap {
         headerInfo[15] = getRealValue((biCrlImportantBytes));
         return headerInfo;
     }
->>>>>>> secundaria
+
 
     private int[] cutArray(int[] arrayToCut, int from, int to) {
         int[] newLength = new int[(to - from) + 1];
